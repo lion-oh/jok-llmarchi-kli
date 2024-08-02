e6246ab38e0b0749405ff4c9041a32b2d8fdd8df
hyrok2 <hyrok2@douzone.com>
2024-08-02 21:24:05 +0900

    fix: lora config 수정

--- src/configs/peft.py
+++ src/configs/peft.py
@@ -3,8 +3,8 @@ from typing import List
 
 @dataclass
 class lora_config:
-    r: int=16
-    lora_alpha: int=32
+    r: int=4
+    lora_alpha: int=8
     target_modules: List[str] = field(default_factory=lambda: ["q_proj", "v_proj"])
     bias="none"
     task_type: str="CAUSAL_LM"
