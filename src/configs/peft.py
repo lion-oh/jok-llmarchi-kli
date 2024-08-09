cb7ce50ce2d81ccc561ffaefce810350ed11403a
jihoon99 <jihoonjung99@gmail.com>
2024-08-09 18:25:48 +0900

    fix : default config as LoRA, if you want to use MoRA change ignored code on config/peft

--- src/configs/peft.py
+++ src/configs/peft.py
@@ -1,22 +1,22 @@
 from dataclasses import dataclass, field
 from typing import List
 
-# @dataclass
-# class lora_config:
-#     r: int=16
-#     lora_alpha: int=32
-#     target_modules: List[str] = field(default_factory=lambda: ['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj'])
-#     bias="none"
-#     task_type: str="CAUSAL_LM"
-#     lora_dropout: float=0.05
-
 @dataclass
 class lora_config:
-    use_mora: bool = True
-    mora_type: int=6
     r: int=16
     lora_alpha: int=32
     target_modules: List[str] = field(default_factory=lambda: ['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj'])
     bias="none"
     task_type: str="CAUSAL_LM"
     lora_dropout: float=0.05
+
+# @dataclass
+# class lora_config:
+#     use_mora: bool = True
+#     mora_type: int=6
+#     r: int=8
+#     lora_alpha: int=16
+#     target_modules: List[str] = field(default_factory=lambda: ['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj'])
+#     bias="none"
+#     task_type: str="CAUSAL_LM"
+#     lora_dropout: float=0.05
